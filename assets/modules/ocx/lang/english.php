323412a922a1dee8e673ececcbb2f47164f4d121
Nicola Lambathakis <modxit@modx.it>
2017-02-10 15:24:04 +0100

    english lang

---
+++ assets/modules/ocx/lang/english.php
@@ -0,0 +1,81 @@
+<?php
+// ---------------------------------------------------------------
+// :: OCx
+// ----------------------------------------------------------------
+//
+// 	Short Description:
+//         OCx Opencart Module
+//
+//   Version:
+//         1.8
+//
+//   Created by:
+// 	    Nicola Lambathakis http://www.tattoocms.it/
+//
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+
+$_lang['modulename'] = 'OCx';
+$_lang['OpencartModule'] = 'Opencart Module';
+$_lang["Dashboard"] = 'Dashboard';
+$_lang['Connection_Info'] = 'Connected to your shop at';
+
+$_lang["loading_data"] = 'Loading data from your Opencart Shop';
+$_lang["wait"] = 'Please wait';
+$_lang["opencart"] = 'OpenCart';
+$_lang["Config"] = 'Configuration';
+$_lang["close"] = 'Close';
+
+$_lang["search"] = 'Search';
+$_lang["all"] = 'All';
+$_lang["infos"] = 'Showing {{ctx.start}} to {{ctx.end}} of {{ctx.total}} entries';
+
+$_lang["snippet_call"] = 'snippet';
+
+//Products
+
+$_lang['Products'] = 'Products';
+$_lang['Categories'] = 'Categories';
+$_lang['Orders'] = 'Orders';
+$_lang['Customers'] = 'Customers';
+$_lang['image'] = 'image';
+$_lang['product_id'] = 'product ID';
+$_lang['name'] = 'name';
+$_lang['model'] = 'model';
+$_lang['price'] = 'price';
+$_lang['quantity'] = 'quantity';
+$_lang['date_added'] = 'date added';
+$_lang['date_modified'] = 'date modified';
+$_lang['view'] = 'view';
+
+//categories
+
+$_lang['category_id'] = 'category ID';
+
+//orders
+
+$_lang['email'] = 'email';
+$_lang['order_id'] = 'order id';
+$_lang['status'] = 'status';
+$_lang['currency'] = 'currency';
+$_lang['total'] = 'total';
+$_lang['method'] = 'method';
+$_lang['firstname'] = 'firstname';
+$_lang['lastname'] = 'lastname';
+$_lang['telephone'] = 'telephone';
+$_lang['address'] = 'address';
+$_lang['city'] = 'city';
+$_lang['postcode'] = 'postcode';
+
+//customers
+$_lang['customer_id'] = 'customer id';
+$_lang['ip'] = 'ip';
+$_lang['manager_themes'] = 'manager themes';
+
+
+
