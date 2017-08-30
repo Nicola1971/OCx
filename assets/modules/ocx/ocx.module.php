d926358386f376b13cf71781f3fcfd03aee7fc43
Nicola <modxit@modx.it>
2017-08-30 13:06:42 +0200

    v1.9 for Evolution cms 1.3.x

--- assets/modules/ocx/ocx.module.php
+++ assets/modules/ocx/ocx.module.php
@@ -3,7 +3,7 @@
 /**
  * OCx
  *
- * OCx 1.8 Module - Open Cart Dashboard
+ * OCx 1.9 Module - Open Cart Dashboard
  * @author	Nicola Lambathakis
  * @category	module
  * @internal	@modx_category OCx
@@ -41,7 +41,7 @@ $test = "test";
 $moduleurl = 'index.php?a=112&id='.$_GET['id'].'&';
 
 // module info
-$module_version = '1.8';
+$module_version = '1.9';
 $module_id = (!empty($_REQUEST["id"])) ? (int)$_REQUEST["id"] : $yourModuleId;
 $mods_path = $modx->config['base_path'] . "assets/modules/";
 
@@ -200,11 +200,11 @@ jQuery(document).ready(function ($) {
     formatters: {
             "code1": function(column, row)
         {
-            return "<textarea rows=\"4\" cols=\"30\">[[OCxProduct? &id=`" + row.product_id + "` &opencartTpl=`opencartTpl` &fetchimages=`1` &store_dir=`assets/images/ocx`]]</textarea>";
+            return "<textarea rows=\"3\" cols=\"30\">[[OCxProduct? &id=`" + row.product_id + "` &opencartTpl=`opencartTpl` &fetchimages=`1` &store_dir=`assets/images/ocx`]]</textarea>";
         },
             "code2": function(column, row)
         {
-            return "<textarea rows=\"4\" cols=\"40\">[[OCxCategory? &cat=`" + row.category_id + "` &opencartTpl=`opencartTpl` &limit=`50` &fetchimages=`1` &orderby=`price` &store_dir=`assets/images/ocx` &orderdir=`DESC` ]]</textarea>";
+            return "<textarea rows=\"3\" cols=\"40\">[[OCxCategory? &cat=`" + row.category_id + "` &opencartTpl=`opencartTpl` &limit=`50` &fetchimages=`1` &orderby=`price` &store_dir=`assets/images/ocx` &orderdir=`DESC` ]]</textarea>";
         },
         "imageCol": function(column, row)
         {
@@ -212,11 +212,11 @@ jQuery(document).ready(function ($) {
         },
 		 "viewproduct": function(column, row)
         {
-            return "<a target=\"_blank\" class=\"btn btn-sm btn-default command-edit\" data-row-id=\"" + row.id + "\" href=\"'.$shop_url.'/index.php?route=product/product&product_id=" + row.product_id + "\"><span class=\"fa fa-eye\"></span></a> ";
+            return "<a target=\"_blank\" class=\"btn btn-sm btn-primary command-edit\" data-row-id=\"" + row.id + "\" href=\"'.$shop_url.'/index.php?route=product/product&product_id=" + row.product_id + "\"><span class=\"fa fa-eye\"></span></a> ";
         },
     		 "viewcat": function(column, row)
         {
-            return "<a target=\"_blank\" class=\"btn btn-sm btn-default command-edit\" data-row-id=\"" + row.id + "\" href=\"'.$shop_url.'/index.php?route=product/category&path=" + row.category_id + "\"><span class=\"fa fa-eye\"></span></a> ";
+            return "<a target=\"_blank\" class=\"btn btn-sm btn-primary command-edit\" data-row-id=\"" + row.id + "\" href=\"'.$shop_url.'/index.php?route=product/category&path=" + row.category_id + "\"><span class=\"fa fa-eye\"></span></a> ";
         }
     }
     });
@@ -228,26 +228,12 @@ jQuery(document).ready(function ($) {
 .codeColumn2 {min-width:350px; margin-right:3px;}
 textarea {font-size:11px; border:dotted 1px #3697CD;}
 .actionBar li {list-style-type: none; list-style-image:none;}
+
+.pagination{display:inline-block;padding-left:0;margin:20px 0;border-radius:4px}.pager li,.pagination>li{display:inline}.pagination>li>a,.pagination>li>span{position:relative;float:left;padding:6px 12px;margin-left:-1px;line-height:1.42857143;color:#337ab7;text-decoration:none;background-color:#fff;border:1px solid #ddd}.pagination>li:first-child>a,.pagination>li:first-child>span{margin-left:0;border-top-left-radius:4px;border-bottom-left-radius:4px}.pagination>li:last-child>a,.pagination>li:last-child>span{border-top-right-radius:4px;border-bottom-right-radius:4px}.pagination>li>a:focus,.pagination>li>a:hover,.pagination>li>span:focus,.pagination>li>span:hover{z-index:2;color:#23527c;background-color:#eee;border-color:#ddd}.pagination>.active>a,.pagination>.active>a:focus,.pagination>.active>a:hover,.pagination>.active>span,.pagination>.active>span:focus,.pagination>.active>span:hover{z-index:3;color:#fff;cursor:default;background-color:#337ab7;border-color:#337ab7}.pagination>.disabled>a,.pagination>.disabled>a:focus,.pagination>.disabled>a:hover,.pagination>.disabled>span,.pagination>.disabled>span:focus,.pagination>.disabled>span:hover{color:#777;cursor:not-allowed;background-color:#fff;border-color:#ddd}.pagination-lg>li>a,.pagination-lg>li>span{padding:10px 16px;font-size:18px;line-height:1.3333333}.pagination-lg>li:first-child>a,.pagination-lg>li:first-child>span{border-top-left-radius:6px;border-bottom-left-radius:6px}.pagination-lg>li:last-child>a,.pagination-lg>li:last-child>span{border-top-right-radius:6px;border-bottom-right-radius:6px}.pagination-sm>li>a,.pagination-sm>li>span{padding:5px 10px;font-size:12px;line-height:1.5}.pagination-sm>li:first-child>a,.pagination-sm>li:first-child>span{border-top-left-radius:3px;border-bottom-left-radius:3px}.pagination-sm>li:last-child>a,.pagination-sm>li:last-child>span{border-top-right-radius:3px;border-bottom-right-radius:3px}.pager{padding-left:0;margin:20px 0;text-align:center;list-style:none}.pager li>a,.pager li>span{display:inline-block;padding:5px 14px;background-color:#fff;border:1px solid #ddd;border-radius:15px}.pager li>a:focus,.pager li>a:hover{text-decoration:none;background-color:#eee}.pager .next>a,.pager .next>span{float:right}.pager .previous>a,.pager .previous>span{float:left}.pager .disabled>a,.pager .disabled>a:focus,.pager .disabled>a:hover,.pager .disabled>span{color:#777;cursor:not-allowed;background-color:#fff}
 </style>
 </head>
 <body>
-<div id="preLoader"><table width="100%" border="0" cellpadding="0"><tr><td align="center">
-    <div class="preLoaderText" style="width:450px;height:300px">
-<h1 class="pagetitle">
-  <span class="pagetitle-icon">
-     <span class="fa-stack fa-lg fa-pulse">
-  <i class="fa fa-circle fa-stack-2x"></i>
-  <i class="fa fa-opencart fa-stack-1x fa-inverse"></i>
-</span>
-  </span>
-  <span class="pagetitle-text">
- OCx Opencart 
-  </span>
-</h1>
-<p>
-'.$_lang["loading_data"].'<br/>'.$_lang["wait"].'               
-</p>
-</div>
+<div id="mainloader"></div>
     </td>
     </tr>
     </table>
